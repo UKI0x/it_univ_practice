9b111628dd4370bdd25be915a263d948f681f82a
UKI0x <ono.oooo.syty@gmail.com>
2021-01-28 20:23:14 -0900

    Initial commit
    練習です

---
+++ page.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<title>Mytheme</title>
+  </head>
+  <body>
+	  <header>
+	  <div class="container">
+		<h1 class="h1 py-3">Mytheme</h1>
+	  </div>
+	
+	  </header>
+	  <!--グローバルナビゲーション-->
+	  
+	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+		<div class="container">
+		<a class="navbar-brand text-white" href="#">Home</a>
+		<button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+		<li class="nav-item active">
+		<a class="nav-link text-white" href="#">WordPress</a>
+		</li>
+		<li class="nav-item active">
+		<a class="nav-link text-white" href="#">HTML</a>
+		</li>
+		<li class="nav-item active">
+		<a class="nav-link text-white" href="#">CSS</a>
+		</li>
+		<li class="nav-item active">
+		<a class="nav-link text-white" href="#">PHP</a>
+		</li>
+		</ul>
+		</div>
+		</div>
+	</nav>
+	<main class="bg-light">
+		<div class="container">
+		
+		<div class="row py-3">
+			<!--メインコンテンツ-->
+			<div class="col-md-10 col-12 mx-auto">
+				<div class="bg-white py-3 text-center">
+					<!--日付-->
+					<p>2021/1/27</p>
+					<!--記事タイトル-->
+					<h1 class="h2 px-3 pb-3 font-weight-bold">ピックアップコンテンツ1ピックアップコンテンツ1ピックアップコンテンツ1</h1>
+					<!--カテゴリー-->
+					<p><a href="">Wordpress</a></p>
+					<!--サムネイル-->
+					<div  class="pb-3">
+					<img src="img/img04.jpg" alt="" class="img-fluid">
+					</div>
+					<!--本文-->
+					<div class="text-left px-2">
+						サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+						サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+						サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+						サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<footer class="bg-white"> 
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-12">
+						<div class="pb-5">
+							<h4 class="d-inline-block py-3 border-bottom border-info">About</h4>
+						</div>
+						<p>
+							テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+						</p>
+					</div>
+					<div class="col-md-4 col-12">
+						<div class="pb-5">
+							<h4 class="d-inline-block py-3 border-bottom border-info">Portfolio</h4>
+						</div>
+						<div class="p-3 border-top border-bottom border-secondary">
+							<a class="text-secondary" href="">ポートフォリオサイト</a>
+						</div>
+						<div class="p-3 border-top border-bottom border-secondary">
+							<a class="text-secondary" href="">ポートフォリオサイト</a>
+						</div>
+					</div>
+					<div class="col-md-4 col-12">
+						<div class="pb-5">
+							<h4 class="d-inline-block py-3 border-bottom border-info">Twitter</h4>
+							<a class="twitter-timeline" data-lang="ja" data-height="600" href="https://twitter.com/uki1x?ref_src=twsrc%5Etfw">Tweets by uki1x</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+						</div>
+					</div>
+				</div>
+				
+
+			</div>
+			<div class="bg-dark text-white p-3 text-center">
+				<p>Copyright -ukicreate,2021 All Rights Reserved.</p>
+			</div>
+		</footer>
+	</main>
+	
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
